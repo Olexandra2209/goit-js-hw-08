7b89f61fc8dbf1f2f12524126339a025015305c3
Olexandra2209 <123394674+Olexandra2209@users.noreply.github.com>
2023-06-24 23:28:16 +0300

    add three

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector('.gallery');
+const imagesMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+formRef.addEventListener('input', throttle(handleFormInput, 500));
+
+function handleFormInput() {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value.trim(),
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+function formFields() {
+  const storedData = localStorage.getItem(STORAGE_KEY);
+  if (storedData) {
+    const formData = JSON.parse(storedData);
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+}
+
+formRef.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.currentTarget.reset();
+
+  localStorage.removeItem(STORAGE_KEY);
+  emailInput.value = '';
+  messageInput.value = '';
+
+  console.log('Form Data:', formData);
+}
